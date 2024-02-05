71559881a5cc762943f6d6294400ff0b56d79bab
Ngoc Thinh <thinhdnde150052@fpt.edu.com>
2024-02-05 17:27:50 +0700

    Fix Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/pages/TrackingXe_TrangThai.dart';
-// import 'package:project/pages/MainMenu.dart';
-// import 'package:project/pages/Guess.dart';
-// import 'package:project/pages/Login.dart';
-import 'package:project/pages/NhanXe.dart';
-import 'package:project/pages/NhanXe2.dart';
-import 'package:project/pages/TrackingXe_Vitri.dart';
-import 'package:project/widgets/widget_body/map.dart';
 import 'package:project/pages/home.dart';
-// import 'package:project/pages/MainMenu.dart';
-// import 'package:project/pages/QLKhoXe.dart';
 
 void main() {
   runApp(MyApp());
